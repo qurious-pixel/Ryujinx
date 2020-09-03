4caa7af28556a770ee50bb90b10bd5dbcb119733
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-02 17:37:55 -0700

    Update update.sh

--- .travis/update.sh
+++ .travis/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-zenity --question --timeout=10 --title="Ryujinx updater" --text="New update available. Update now?" --icon-name=Ryujinx --window-icon=Ryujinx.png --height=80 --width=400
+$APPDIR/usr/bin/zenity --question --timeout=10 --title="Ryujinx updater" --text="New update available. Update now?" --icon-name=Ryujinx --window-icon=Ryujinx.png --height=80 --width=400
 answer=$?
 
 if [ "$answer" -eq 0 ]; then 
-	$APPDIR/usr/bin/AppImageUpdate $PWD/Ryujinx-x86_64.AppImage
+	$APPDIR/usr/bin/AppImageUpdate $PWD/Ryujinx-x86_64.AppImage && $PWD/Ryujinx-x86_64.AppImage
 elif [ "$answer" -eq 1 ]; then
 	$APPDIR/AppRun-patched
 elif [ "$answer" -eq 5 ]; then
