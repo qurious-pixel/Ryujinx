f6c2bffd63e221f70d75df2a5e943ae5a0577b85
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-10 12:09:38 -0700

    Update appimage.sh
    cleanup script

--- appimage.sh
+++ appimage.sh
@@ -21,15 +21,4 @@ chmod a+x appimagetool-x86_64.AppImage
 
 ./appimagetool-x86_64.AppImage AppDir/
 ./appimagetool-x86_64.AppImage --appimage-extract
-#export PATH="$PATH:/c/projects/ryujinx/squashfs-root/usr/bin"
-#echo $PATH
-#wsl ./squashfs-root/usr/bin/appimagetool AppDir/
-#wsl ./squashfs-root/usr/bin/desktop-file-validate AppDir/Ryujinx.desktop
-
-#docker run --help
-#docker pull --platform=linux appimagecrafters/appimage-builder:latest
-#docker run --platform=linux --mount type=bind,source="$(pwd)",target=/app appimagecrafters/appimage-builder:latest /bin/bash ls -al
-#docker run -e COMPOSE_CONVERT_WINDOWS_PATHS=1 appimagecrafters/appimage-builder:latest appimagetool-x86_64.AppImage AppDir/
-#docker pull --platform=linux ubuntu:latest
-#docker run --platform=linux --mount type=bind,source="$(pwd)",target=/app ubuntu:latest
-
+echo ryujinx%config_name%%APPVEYOR_BUILD_VERSION%-linux_x64
