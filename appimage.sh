12ce383632caf9463789af01c8e7ac4d6b61821b
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-10 09:18:08 -0700

    Update appimage.sh

--- appimage.sh
+++ appimage.sh
@@ -22,8 +22,9 @@ wsl ./appimagetool-x86_64.AppImage --appimage-extract
 export PATH="$PATH:/mnt/c/projects/ryujinx/squashfs-root/usr/bin"
 echo $PATH
 wsl ./squashfs-root/usr/bin/appimagetool AppDir/
+wsl ./squashfs-root/usr/bin/desktop-file-validate AppDir/
 
-#docker pull appimagecrafters/appimage-builder:latest
-#wsl docker run -v $(pwd):/Ryujinx appimagecrafters/appimage-builder:latest appimagetool-x86_64.AppImage AppDir/
+docker pull appimagecrafters/appimage-builder:latest
+docker run -v $PWD:/Ryujinx appimagecrafters/appimage-builder:latest /bin/bash ls -al
 #docker run -e COMPOSE_CONVERT_WINDOWS_PATHS=1 appimagecrafters/appimage-builder:latest appimagetool-x86_64.AppImage AppDir/
 
