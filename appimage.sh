788465547bb8383fc0b442834bd3e366575f72d8
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-09-10 11:50:01 -0700

    Update appimage.sh

--- appimage.sh
+++ appimage.sh
@@ -34,4 +34,5 @@ chmod a+x appimagetool-x86_64.AppImage
 #docker run -e COMPOSE_CONVERT_WINDOWS_PATHS=1 appimagecrafters/appimage-builder:latest appimagetool-x86_64.AppImage AppDir/
 #docker pull --platform=linux ubuntu:latest
 #docker run --platform=linux --mount type=bind,source="$(pwd)",target=/app ubuntu:latest
-echo $LCOW_SUPPORTED
+bash echo $LCOW_SUPPORTED
+docker info
