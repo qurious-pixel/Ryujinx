1378c5996efbd67743ef760d3a533d6adf13ecf9
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-11-15 20:35:28 -0800

    Update update.sh

--- .travis/update.sh
+++ .travis/update.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-$APPDIR/usr/bin/zenity --question --timeout=10 --title="Ryujinx updater" --text="New update available. Update now?" --icon-name=Ryujinx --window-icon=Ryujinx.png --height=80 --width=400
-answer=$?
+unset LD_LIBRARY_PATH
+if [ -f /usr/bin/zenity ]; then
+	zenity --question --timeout=10 --title="Ryujinx updater" --text="New update available. Update now?" --icon-name=Ryujinx --window-icon=Ryujinx.png --height=80 --width=400
+	answer=$?
+else
+	dialog --title Ryujinx --timeout 10 --yesno "New update available. Update now?" 0 0
+	answer=$?
+fi
+
 
 if [ "$answer" -eq 0 ]; then 
+	export LD_PRELOAD="$APPDIR/usr/lib/updater/libcurl.so.4"
 	$APPDIR/usr/bin/AppImageUpdate $PWD/Ryujinx-x86_64.AppImage && $PWD/Ryujinx-x86_64.AppImage
 elif [ "$answer" -eq 1 ]; then
 	$APPDIR/AppRun-patched
-elif [ "$answer" -eq 5 ]; then
+else 
 	$APPDIR/AppRun-patched
 fi
 exit 0
+
 
