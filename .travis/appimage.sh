dd1ed26e7881dea7054fef2c1446b6c6b7fd6f85
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-12-31 07:42:07 -0800

    Update appimage.sh
    update variables

--- .travis/appimage.sh
+++ .travis/appimage.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -ex
 
+BRANCH=`echo ${GITHUB_REF##*/}`
+
 mkdir -p AppDir/usr/bin
 cp Ryujinx/bin/Release/net5.0/linux-x64/publish/* AppDir/usr/bin/
-curl -sL https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/Ryujinx/Ui/assets/Ryujinx.svg -o AppDir/Ryujinx.svg
-curl -sL https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/Ryujinx.desktop -o AppDir/Ryujinx.desktop
-curl -sL https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.travis/update.sh -o AppDir/update.sh
-curl -sL https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.travis/AppRun -o AppDir/AppRun
+curl -sL https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/Ryujinx/Ui/assets/Ryujinx.svg -o AppDir/Ryujinx.svg
+curl -sL https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/Ryujinx.desktop -o AppDir/Ryujinx.desktop
+curl -sL https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/.travis/update.sh -o AppDir/update.sh
+curl -sL https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/.travis/AppRun -o AppDir/AppRun
 curl -sL https://github.com/RPCS3/AppImageKit-checkrt/releases/download/continuous2/AppRun-patched-x86_64 -o AppDir/AppRun-patched
 curl -sL https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-x86_64 -o ./AppDir/runtime
 mkdir -p AppDir/usr/share/applications && cp ./AppDir/Ryujinx.desktop ./AppDir/usr/share/applications
@@ -16,7 +18,7 @@ mkdir -p AppDir/usr/optional/ ; mkdir -p squashfs-root/usr/optional/libstdc++/
 mkdir -p AppDir/usr/share/zenity 
 cp /usr/share/zenity/zenity.ui ./AppDir/usr/share/zenity/
 cp /usr/bin/zenity ./AppDir/usr/bin/
-curl -sL https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.travis/Config.json -o ./AppDir/usr/share/Config.json
+curl -sL https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/.travis/Config.json -o ./AppDir/usr/share/Config.json
 #curl -sL https://github.com/RPCS3/AppImageKit-checkrt/releases/download/continuous2/exec-x86_64.so -o ./AppDir/usr/optional/exec.so
 #cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 AppDir/usr/optional/libstdc++/
 #printf "#include <bits/stdc++.h>\nint main(){std::make_exception_ptr(0);std::pmr::get_default_resource();}" | $CXX -x c++ -std=c++2a -o $HOME/squashfs-root/usr/optional/checker -
@@ -27,13 +29,13 @@ chmod a+x ./AppDir/runtime
 chmod a+x ./AppDir/usr/bin/Ryujinx
 chmod a+x ./AppDir/update.sh
 
-curl -sLO https://raw.githubusercontent.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.travis/update.tar.gz
+curl -sLO https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/.travis/update.tar.gz
 tar -xzf update.tar.gz
 mv update/AppImageUpdate ./AppDir/usr/bin/
 mkdir -p AppDir/usr/lib/
 mv update/* ./AppDir/usr/lib/
 
-echo $TRAVIS_BUILD_ID > ./AppDir/version.txt
+echo $GITHUB_RUN_ID > ./AppDir/version.txt
 
 
 ls -al ./AppDir
